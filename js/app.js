b357453d54766f0b619badbd9f5f3e9d276524ce
lenonbr <lenon.br@hotmail.com>
2025-03-28 15:51:23 -0300

    inserindo validações

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+
     let lista = document.getElementById('lista-amigos');
+
 
     amigos.push(amigo.value);
 
