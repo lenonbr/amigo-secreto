709457922307b0cfadbaca7401d77a6c2dc279fa
lenonbr <lenon.br@hotmail.com>
2025-03-28 13:48:30 -0300

    criada função para adicionar amigos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
+adicionar();
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let listaAmigos = document.getElementById('lista-amigos');
 
+    if(listaAmigos.textContent == '') {
+        listaAmigos.textContent = amigo.value;
+    }
+    else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+    
 }
 
 function sortear() {
