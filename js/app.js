76307de4899a64e66435c7dee09d094bee8e9df6
lenonbr <lenon.br@hotmail.com>
2025-03-28 13:28:56 -0300

    criando variáveis principais

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let listaAmigos = document.getElementById('lista-amigos');
 
 }
 
 function sortear() {
-
+    let listaSorteio = document.getElementById('lista-sorteio');
+    
 }
 
 function reiniciar() {
-    
+
 }
